df48698dce4ee88a58ee344a3bf16af588d922c0
ConfusionCode <confusioncodes@gmail.com>
2025-08-08 09:18:37 -0700

    Changed custom level renaming to be more intuitive.

--- scripts/custom_level.gd
+++ scripts/custom_level.gd
@@ -3,9 +3,7 @@ class_name CustomLevelButton
 
 const SCENE = preload("res://scenes/custom_level.tscn");
 
-@onready var rename_button: Button = %RenameButton
 @onready var title_edit: LineEdit = %TitleEdit
-@onready var title_label: Label = %TitleLabel
 
 signal level_edited(id: int);
 signal level_deleted(id: int);
@@ -23,27 +21,18 @@ var id: int;
 func set_title(title: String) -> void:
 	title = title.validate_filename();
 	self.title = title;
-	%TitleLabel.text = title;
 	%TitleEdit.text = title;
-
-
-func _on_rename_button_pressed() -> void:
-	title_label.visible = false;
-	title_edit.visible = true;
-	rename_button.disabled = true;
 
 
 func _on_title_text_submitted(new_text: String) -> void:
 	set_title(new_text);
-	title_edit.visible = false;
-	title_label.visible = true;
-	rename_button.disabled = false;
 	level_renamed.emit(id);
 
+func _on_title_edit_focus_exited() -> void:
+	_on_title_text_submitted(title_edit.text);
 
 func _on_delete_button_pressed() -> void:
 	level_deleted.emit(id);
-
 
 func _on_edit_button_pressed() -> void:
 	level_edited.emit(id);
